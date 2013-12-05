add917af00059b181d506b7733d092e9d6c8af82
Milan Toth <milgra@milgra.com>
2013-12-05 14:13:50 +0100

    Project upload

---
+++ Sources/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  Compiler
+//
+//  Created by Milan Toth on 10/17/13.
+//  Copyright (c) 2013 Milan Toth. All rights reserved.
+//
+
+#include <stdio.h>
+#include "clcsrc.h"
+
+int main( 	int			theCount , 
+			const char* theTokens[ ] )
+{
+	struct Main* main = Main_alloc( );
+    int result = Main_init( main , theCount , theTokens );
+    Main_release( main );
+    return result;
+}
+
